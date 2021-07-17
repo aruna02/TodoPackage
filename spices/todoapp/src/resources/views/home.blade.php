c66d90cae4cd0bd0370fb8a7cfc9b7bfc5fcdef2
aruna-spices <[aruna@spicesresearch.com]>
2021-07-17 17:45:05 +0545

    added link and script

--- spices/todoapp/src/resources/views/home.blade.php
+++ spices/todoapp/src/resources/views/home.blade.php
@@ -2,7 +2,10 @@
 
 @section('content')
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <div class="container mb-4">
         <h4>
             <span class="todo-fonts"> Todos </span>
